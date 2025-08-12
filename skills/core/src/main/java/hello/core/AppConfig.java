29007620a9e69eb4210741aa600850059f28903a
219 <lhg2196@naver.com>
2025-08-12 21:16:57 +0900

    스프링 컨테이너와 스프링 빈

--- skills/core/src/main/java/hello/core/AppConfig.java
+++ skills/core/src/main/java/hello/core/AppConfig.java
@@ -2,10 +2,8 @@ package hello.core;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
-import hello.core.member.MemberRepository;
-import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
-import hello.core.member.MemoryMemberRepository;
+import hello.core.member.*;
+import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 import org.springframework.context.annotation.Bean;
@@ -14,19 +12,30 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
+    /*
+    call AppConfig.memberService
+    call AppConfig.memberRepository
+    call AppConfig.orderService
+    이런식으로 뜸 테스트시 이것이 싱글톤을 보장해준다는 소리
+     */
+
+
     @Bean
    public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
        //생성자를 통해서 주입한다.
        return new MemberServiceImpl(memberRepository());
    }
 
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
        return new OrderServiceImpl(memberRepository(), discountPolicy());
    }
 
@@ -35,4 +44,7 @@ public class AppConfig {
        //FixDiscountPolicy -> RateDiscountPolicy 이렇게 바꿈으로써 DI 의존성하고 OCP 지켜짐
        return new RateDiscountPolicy();
     }
+
+
+
 }
